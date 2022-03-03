015d3c5d638615422f4b0bdc903f6d61f6f0bb4f
mvxxx <michal.tyrolski@gmail.com>
2022-03-03 15:08:15 +0100

    Common workspace

--- sync_clusters.py
+++ sync_clusters.py
@@ -8,35 +8,17 @@ from utils.communication import exec_on_rem_workspace
 _USERNAME_REMOTE = 'username@remote'
 _TREE_ROOT = 'tree_root'
 
-def sync_clusters(src_cluster, target_cluster, config):
-    with open(config, 'r') as cfg_f:
-        content = cfg_f.read()
-    cfg = json.loads(content)
-
-    src_cluster_host = cfg['clusters'][src_cluster][_USERNAME_REMOTE]
-    src_tree_root = cfg['clusters'][src_cluster][_TREE_ROOT]
-
+def sync_clusters(target_cluster, config):
     target_cluster_host = cfg['clusters'][target_cluster][_USERNAME_REMOTE]
     target_tree_root = cfg['clusters'][target_cluster][_TREE_ROOT]
 
     sync_paths = cfg['sync_paths']
-    workspace = uuid4().hex
-    os.makedirs(workspace)
+    for sync_path in sync_paths:
+        # rsync from local to remote
+        exec_on_rem_workspace(target_cluster_host, target_tree_root, [f'mkdir -p {sync_path}'])
+        os.system(f'rsync -vuar {os.path.join(workspace, sync_path)}/* {target_cluster_host}:{os.path.join(target_tree_root, sync_path)}')
 
-    try:
-        for sync_path in sync_paths:
-            # rsync from remote to local
-            os.makedirs(os.path.join(workspace, sync_path))
-            os.system(f'rsync -vuar {src_cluster_host}:{os.path.join(src_tree_root, sync_path)}/* {os.path.join(workspace, sync_path)}')
 
-            # rsync from local to remote
-            exec_on_rem_workspace(target_cluster_host, target_tree_root, [f'mkdir -p {sync_path}'])
-            os.system(f'rsync -vuar {os.path.join(workspace, sync_path)}/* {target_cluster_host}:{os.path.join(target_tree_root, sync_path)}')
-
-    except Exception as e:
-        print('Something failed...', str(e))
-    finally:   
-        shutil.rmtree(workspace)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -48,9 +30,30 @@ if __name__ == '__main__':
         '-t', '--target-cluster', action='append', help='Names of clusters to be synced', required=True)
     args = parser.parse_args()
 
-    cfg = args.config
+    config_path = args.config
     source_cluster = args.source_cluster
     target_clusters = args.target_cluster
 
+    workspace = uuid4().hex
+    os.makedirs(workspace)
+    
+    with open(config_path, 'r') as cfg_f:
+        content = cfg_f.read()
+    cfg = json.loads(content)
+
+    src_cluster_host = cfg['clusters'][source_cluster][_USERNAME_REMOTE]
+    src_tree_root = cfg['clusters'][source_cluster][_TREE_ROOT]
+
+    # rsync from remote to local
+    for sync_path in cfg['sync_paths']:
+        os.makedirs(os.path.join(workspace, sync_path))
+        os.system(f'rsync -vuar {src_cluster_host}:{os.path.join(src_tree_root, sync_path)}/* {os.path.join(workspace, sync_path)}')
+    
     for target in target_clusters:
-        sync_clusters(source_cluster, target, cfg)
+        try:
+            sync_clusters(target, cfg)
+        except Exception as e:
+            print(f'Failed syncing to {target} due to exception: {str(e)}')
+
+    print(f'Removing workspace {workspace}')
+    shutil.rmtree(workspace)
